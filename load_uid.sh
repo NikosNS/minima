a29093a6f1377f1503521a102d6acabc2d893066
NikosNS <74458274+NikosNS@users.noreply.github.com>
2022-04-20 19:47:30 +0300

    Delete load_uid.sh

--- load_uid.sh
+++
@@ -1,3 +0,0 @@
-docker exec alp_minima4 curl 127.0.0.1:9002/incentivecash+uid:
-docker exec alp_minima5 curl 127.0.0.1:9002/incentivecash+uid:
-docker exec alp_minima6 curl 127.0.0.1:9002/incentivecash+uid:
